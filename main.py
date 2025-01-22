43489ffe7b616ed517c8463a05a4d7eaebcc31f2
tylermichaelsen <163109530+tylermichaelsen@users.noreply.github.com>
2025-01-22 08:03:05 +0000

    Fix syntax error

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ clock = pygame.time.Clock()
 running = True
 
 while running:
-    for event in pygame.event.get()
+    for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
